90dd32499c569356114045976fbc739bcdd08985
kamakshi1987 <48826736+kamakshi1987@users.noreply.github.com>
2020-07-28 12:54:14 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  username : string
+  password : string
+
+  login() : void {
+    if(this.username == 'admin' && this.password == 'admin'){
+    }else {
+      alert("Invalid credentials");
+    }
+  }
+
+}
